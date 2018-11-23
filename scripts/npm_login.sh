71aee985173731623b8b6ac1b9db5edc5c2779df
Mark Ackert <ackma02@Marks-MBP.fios-router.home>
2018-11-23 15:08:51 -0500

    Switch script shebang
    Signed-off-by: Mark Ackert <ackma02@Marks-MBP.fios-router.home>

--- scripts/npm_login.sh
+++ scripts/npm_login.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ###
 # This program and the accompanying materials are made available under the terms of the
 # Eclipse Public License v2.0 which accompanies this distribution, and is available at
