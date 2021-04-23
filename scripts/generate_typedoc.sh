cf59c2b6e40a97c8ca326d47fc56b2328d25b2d4
Timothy Johnson <timothy.johnson@broadcom.com>
2021-04-23 09:57:06 -0400

    Make Bash script syntax more universal
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/generate_typedoc.sh
+++ scripts/generate_typedoc.sh
@@ -28,7 +28,7 @@ npm install --save-dev @types/node typescript@^3.8.0 typedoc@^0.19.0 \
 
 # Create directory structure for Imperative and SDK packages
 mkdir -p node_modules/@zowe/imperative
-mv imperative/{packages,README.md} node_modules/@zowe/imperative/
+mv imperative/packages imperative/README.md node_modules/@zowe/imperative/
 mv zowe-cli/packages/core node_modules/@zowe/core-for-zowe-sdk
 mv zowe-cli/packages/provisioning node_modules/@zowe/provisioning-for-zowe-sdk
 mv zowe-cli/packages/zosconsole node_modules/@zowe/zos-console-for-zowe-sdk
