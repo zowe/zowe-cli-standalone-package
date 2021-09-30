91786c2a80ff82cc5b44ace9738384e51151f286
Timothy Johnson <timothy.johnson@broadcom.com>
2021-09-30 17:11:49 -0400

    Load versions from yaml file
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ scripts/load-zowe-versions.js
@@ -0,0 +1,18 @@
+const childProcess = require("child_process");
+const fs = require("fs");
+const jsYaml = require("js-yaml");
+
+const zoweVersions = jsYaml.load(fs.readFileSync(__dirname + "/../zowe-versions.yaml", "utf-8"));
+const outputs = { zowe: zoweVersions.zowe };
+for (const [k, v] of Object.entries(zoweVersions["zowe-cli"])) {
+    outputs[`zowe-${k}`] = v;
+}
+for (const [k, v] of Object.entries(zoweVersions["zowe-sdk"])) {
+    outputs[`zowe-${k}-sdk`] = v;
+}
+for (const [k, v] of Object.entries(zoweVersions["zowe-plugins"])) {
+    outputs[`zowe-${key}-plugin`] = v;
+}
+for (const [k, v] of Object.entries(outputs)) {
+    childProcess.execSync("echo", ["::set-output", `name=${k}::${v}`]);
+}
