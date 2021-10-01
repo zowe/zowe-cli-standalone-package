54389713a1a82814c4d0e77f5b4e900018e74c64
Timothy Johnson <timothy.johnson@broadcom.com>
2021-10-01 12:48:42 -0400

    Compute zowe bundle version dynamically
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/load-zowe-versions.js
+++ scripts/load-zowe-versions.js
@@ -1,5 +1,5 @@
-const childProcess = require("child_process");
 const fs = require("fs");
+const core = require("@actions/core");
 const jsYaml = require("js-yaml");
 
 const zoweVersions = jsYaml.load(fs.readFileSync(__dirname + "/../zowe-versions.yaml", "utf-8"));
@@ -14,5 +14,5 @@ for (const [k, v] of Object.entries(zoweVersions["zowe-plugins"])) {
     outputs[`zowe-${k}-plugin`] = v;
 }
 for (const [k, v] of Object.entries(outputs)) {
-    childProcess.execSync("echo", ["::set-output", `name=${k}::"${v}"`]);
+    core.setOutput(k, v);
 }
