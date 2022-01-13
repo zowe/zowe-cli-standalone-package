daae45841c24ba189cd4af1e84667e8a32e798c3
Timothy Johnson <timothy.johnson@broadcom.com>
2022-01-13 15:35:24 -0500

    Load RC number from zowe-versions.yaml
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/load-zowe-versions.js
+++ scripts/load-zowe-versions.js
@@ -3,7 +3,10 @@ const core = require("@actions/core");
 const jsYaml = require("js-yaml");
 
 const zoweVersions = jsYaml.load(fs.readFileSync(__dirname + "/../zowe-versions.yaml", "utf-8"));
-const outputs = { zowe: zoweVersions.zowe };
+const outputs = {
+    "zowe-semver": zoweVersions.zowe.semver,
+    "zowe-rc": zoweVersions.zowe.rc
+};
 for (const [k, v] of Object.entries(zoweVersions["zowe-cli"])) {
     outputs[`zowe-${k}`] = v;
 }
