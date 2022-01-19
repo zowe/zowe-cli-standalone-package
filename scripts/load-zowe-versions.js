18518c164888670263840f58e4bdaa15d67bfe98
Timothy Johnson <timothy.johnson@broadcom.com>
2022-01-19 15:21:20 -0500

    Add zowe-v1-lts and next sections to yaml file
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/load-zowe-versions.js
+++ scripts/load-zowe-versions.js
@@ -1,21 +1,9 @@
 const fs = require("fs");
 const core = require("@actions/core");
+const flat = require("flat");
 const jsYaml = require("js-yaml");
 
 const zoweVersions = jsYaml.load(fs.readFileSync(__dirname + "/../zowe-versions.yaml", "utf-8"));
-const outputs = {
-    "zowe-version": zoweVersions.zowe.version,
-    "zowe-rc": zoweVersions.zowe.rc
-};
-for (const [k, v] of Object.entries(zoweVersions["zowe-cli"])) {
-    outputs[`zowe-${k}`] = v;
-}
-for (const [k, v] of Object.entries(zoweVersions["zowe-sdk"])) {
-    outputs[`zowe-${k}-sdk`] = v;
-}
-for (const [k, v] of Object.entries(zoweVersions["zowe-plugins"])) {
-    outputs[`zowe-${k}-plugin`] = v;
-}
-for (const [k, v] of Object.entries(outputs)) {
+for (const [k, v] of Object.entries(flat(zoweVersions))) {
     core.setOutput(k, v);
 }
