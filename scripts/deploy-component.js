9cf85dd7470ab05a02cd938cc9ec10ef1caff446
Timothy Johnson <timothy.johnson@broadcom.com>
2022-01-21 10:21:48 -0500

    Add logic to skip publishing packages
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/deploy-component.js
+++ scripts/deploy-component.js
@@ -3,6 +3,9 @@ const os = require("os");
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 const delay = require("delay");
+const jsYaml = require("js-yaml");
+const moment = require("moment");
+const fetch = require("node-fetch");
 
 const pkgScope = "@zowe";
 const sourceRegistry = "https://zowe.jfrog.io/zowe/api/npm/npm-local-release/";
@@ -28,12 +31,37 @@ function npmLogin() {
     fs.appendFileSync(os.homedir() + "/.npmrc", lines.join("\n"));
 }
 
+async function shouldSkipPublish(pkgName, pkgTag, pkgVersion) {
+    const zoweVersions = jsYaml.load(fs.readFileSync(__dirname + "/../zowe-versions.yaml", "utf-8"));
+    const releasesYaml = await fetch("https://raw.githubusercontent.com/zowe/zowe.github.io/master/_data/releases.yml", {
+        headers: { Authorization: `Bearer ${process.env.GITHUB_TOKEN}` }
+    });
+    const releasesData = jsYaml.load(releasesYaml.body);
+    const isStaging = zoweVersions["zowe-v1-lts"].version > releasesData[0].version;
+    if (!isStaging) {
+        return false;
+    } else if (pkgTag !== "next") {
+        const stagedVersion = zoweVersions.packages[pkgName];
+        return pkgVersion > stagedVersion;
+    } else {
+        const dateString = pkgVersion.split(".").pop();
+        const pkgDate = moment(`${dateString.slice(0, 4)}-${dateString.slice(4, 6)}-${dateString.slice(6, 8)}`);
+        return pkgDate.isSameOrAfter(moment().startOf("day"));
+    }
+}
+
 (async () => {
     const pkgName = process.argv[2];
     const pkgTag = process.argv[3];
     fs.rmSync(__dirname + "/../.npmrc", { force: true });
 
     const pkgVersion = getPackageInfo(`${pkgScope}/${pkgName}@${pkgTag}`, viewOpts, "version");
+    if (shouldSkipPublish(pkgName, pkgTag, pkgVersion)) {
+        core.info(`Package ${pkgScope}/${pkgName}@${pkgVersion} will not be published until the next Zowe release.\n` +
+            `To publish it immediately, update the package version in the zowe-versions.yaml file.`);
+        process.exit();
+    }
+
     const tgzUrl = getPackageInfo(`${pkgScope}/${pkgName}@${pkgTag}`, viewOpts, "dist.tarball");
     const fullPkgName = `${pkgName}-${pkgVersion}.tgz`;
     await exec.exec("curl", ["-fs", "-o", fullPkgName, tgzUrl]);
@@ -83,7 +111,7 @@ function npmLogin() {
     }
     if (installError != null) {
         if (oldPkgVersion != null) {
-            echo `Install failed, reverting tag ${pkgTag} to v${oldPkgVersion}`;
+            core.info(`Install failed, reverting tag ${pkgTag} to v${oldPkgVersion}`);
             await exec.exec("npm", ["dist-tag", "add", `${pkgScope}/${pkgName}@${oldPkgVersion}`, pkgTag]);
         }
         throw installError;
