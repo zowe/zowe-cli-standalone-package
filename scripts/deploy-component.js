6ceff27ea9b5d2c85487a71cbf7c1fad49e4e9f6
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-01-31 18:59:49 +0000

    Add rewrite-sw script
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- scripts/deploy-component.js
+++ scripts/deploy-component.js
@@ -1,3 +1,13 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
 const fs = require("fs");
 const os = require("os");
 const core = require("@actions/core");
@@ -8,28 +18,12 @@ const moment = require("moment");
 const fetch = require("node-fetch");
 const { serializeError } = require("serialize-error");
 
+const getPackageInfo = require(__dirname + "/get-package-info").getPackageInfo;
+
 const PKG_SCOPE = "@zowe";
 const SOURCE_REGISTRY = "https://zowe.jfrog.io/zowe/api/npm/npm-local-release/";
 const TARGET_REGISTRY = process.env.NPM_REGISTRY || "https://registry.npmjs.org/";
 const VIEW_OPTS = `--${PKG_SCOPE}:registry=${SOURCE_REGISTRY}`;
-
-async function getPackageInfo(pkg, opts="", prop="version") {
-    core.info(`Getting '${prop}' for package: ${pkg}`);
-    const viewArgs = ["view", pkg, prop];
-    if (opts) {
-        viewArgs.push(opts);
-    }
-    let cmdOutput;
-    try {
-        cmdOutput = (await exec.getExecOutput("npm", viewArgs)).stdout.trim();
-    } catch {
-        throw new Error(`Package not found: ${pkg}`);
-    }
-    if (cmdOutput.length === 0) {
-        throw new Error(`Property not found: ${prop}`);
-    }
-    return cmdOutput;
-}
 
 async function shouldSkipPublish(pkgName, pkgTag, pkgVersion) {
     const response = await fetch("https://raw.githubusercontent.com/zowe/zowe.github.io/master/_data/releases.yml", {
--- scripts/get-next-version.js
+++ scripts/get-next-version.js
@@ -1,3 +1,13 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
 // For snapshot builds, just use @next tag and ignore publish date
 if (process.argv[3] === "next") {
     process.stdout.write(`${process.argv[2]}@${process.argv[3]}`);
