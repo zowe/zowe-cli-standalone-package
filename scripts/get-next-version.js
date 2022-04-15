e7a32709d900170b249361bcf48bfff453ac8166
Timothy Johnson <timothy.johnson@broadcom.com>
2022-04-15 14:41:33 -0400

    Fix incorrect package version of CLI
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/get-next-version.js
+++ scripts/get-next-version.js
@@ -18,18 +18,18 @@ const childProcess = require("child_process");
 const moment = require("moment");
 
 const packageName = process.argv[2];
-const snapshotDate = moment(process.argv[3]);
+const snapshotDate = moment.utc(process.argv[3]);
 
 const packageVersions = JSON.parse(childProcess.execSync(`npm view ${packageName} time --json`));
 let latestVersion;
-let latestTime = moment(0);
+let latestTime = moment.utc(0);
 for (const [version, time] of Object.entries(packageVersions)) {
     // We give priority to versions that:
     // (1) Include "next" in their name
     // (2) Have a publish date older than or the same as the snapshot date
     // (3) Have the newest publish date that meets the above constraints
-    const versionTime = moment(time);
-    if (version.includes("next") && versionTime.startOf("day").isSameOrBefore(snapshotDate) && versionTime.isAfter(latestTime)) {
+    const versionTime = moment.utc(time);
+    if (version.includes("next") && versionTime.clone().startOf("day").isSameOrBefore(snapshotDate) && versionTime.isAfter(latestTime)) {
         latestVersion = version;
         latestTime = versionTime;
     }
