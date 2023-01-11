b2abc0b6532c3da3a644d7bc2ceb6b60a2d24544
Andrew W. Harn <andrew.harn@broadcom.com>
2023-01-11 09:29:55 -0500

    First attempt at reproducing build results
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- scripts/generate_typedoc.sh
+++ scripts/generate_typedoc.sh
@@ -88,4 +88,7 @@ EOF
 
 # Build typedoc and zip it up
 npx typedoc ./node_modules/@zowe
-zip -rX ../zowe-node-sdk-typedoc.zip typedoc
+TZ=UTC find typedoc/ -exec touch -t 197001010000.00 {} +
+TZ=UTC touch -t 197001010000.00 typedoc
+TZ=UTC zip -roX ../zowe-node-sdk-typedoc.zip typedoc
+TZ=UTC touch -t 197001010000.00 ../zowe-node-sdk-typedoc.zip
