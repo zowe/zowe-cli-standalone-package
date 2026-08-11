60becd49ccdabfebf117ebeed793517fe0842dd1
Chris B <chris.boehm@broadcom.com>
2026-08-11 08:16:28 -0400

    Update repackage_bundle.sh with mkdir -p
    Signed-off-by: Chris B <chris.boehm@broadcom.com>

--- scripts/repackage_bundle.sh
+++ scripts/repackage_bundle.sh
@@ -16,7 +16,7 @@ mkdir -p packed
 # Loop through each tar (representing an `npm pack`), and create new tars with packed dependencies.
 for tar in "$@"
 do
-    mkdir temp
+    mkdir -p temp
     tar xzf $tar -C temp
 
     # Changes the package.json format
