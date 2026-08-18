b6879301b10c6cc1b419f8e0d5c91eb8ee508777
Timothy Johnson <timothy.johnson@broadcom.com>
2026-08-18 10:57:32 -0400

    Skip repackaging if dependencies are already bundled (#434)
    * Remove postpack script when packaging CLI
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Skip repackaging when deps are bundled
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Remove postpack as extra safety measure
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    ---------
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/repackage_tar.sh
+++ scripts/repackage_tar.sh
@@ -22,19 +22,28 @@ tarfile=$1;
 registry=$2;
 newversion=$3;
 
-mkdir -p temp
+mkdir temp
 tar xzf $tarfile -C temp
 cd temp
 cd package
+
+# Skip repackaging if dependencies are already bundled
+if (grep -q '"bundleDependencies":' package.json); then
+    rm -rf ../../temp/
+    exit 0
+fi
+
 # Unholy one liner which replace registry and repository with blank strings. Should convert this to javascript file soonTM.
 # Also remove prepare script which may require dev dependencies like Husky - https://github.com/typicode/husky/issues/914
-# Also remove prepack script which may require scripts from the project repo
+# Also remove pre-/post-pack scripts which may require scripts from the project repo
+# TODO Pre-/post-pack scripts can be restored after CLI V2 is updated to stop using them
 # Takes in package.json, outputs package_new.json
 node -e "package = require('./package.json');
     package.publishConfig.registry='$registry';
     package.version='$newversion';
     delete package.scripts.prepare;
     delete package.scripts.prepack;
+    delete package.scripts.postpack;
     require('fs').writeFileSync('package_new.json', JSON.stringify(package, null, 2), 'utf8')"
 # Move the old package JSON to build dir so we can publish as a Jenkins artifact?
 mv package.json ../../$tarfile.json
